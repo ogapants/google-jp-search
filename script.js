6b162b47eb5a792d568b28693b1696acfae70ed2
ogapants <ogapamyu@gmail.com>
2019-11-10 00:35:10 +0900

    support lang_en

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
 	var toolDiv = $("#hdtb-msb > div:nth-child(2)")
 
+	var enElement = document.createElement('a')
+	enElement.setAttribute('class', 'hdtb-tl')
+	enElement.setAttribute('href', document.URL + '&lr=lang_en')
+	enElement.textContent = "EN"
+	toolDiv.after(enElement)
+
 	var jpElement = document.createElement('a')
 	jpElement.setAttribute('class', 'hdtb-tl')
 	jpElement.setAttribute('href', document.URL + '&lr=lang_ja')
 	jpElement.textContent = "JP"
-
 	toolDiv.after(jpElement)
 });
